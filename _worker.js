f24bbce226996bc78dba708346886bb119225efd
hidayatkepin19 <161795587+hidayatkepin19@users.noreply.github.com>
2024-03-06 01:32:21 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '029a86e6-2a75-4077-b413-0af4d71c897a';
 
-const proxyIPs = ['138.201.182.222 '];
+const proxyIPs = ['213.183.51.219 '];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
